6d7b0c63cc370edd7bf0a0a68b0942dfeaa05f5a
Emery <iing7050737@gmail.com>
2025-05-24 19:28:28 +0800

    feat: 服務詳細頁畫面

---
+++ src/router/modules/freelancer-list-detail.js
@@ -0,0 +1,9 @@
+import FreelancerServiceDetail from '@/views/FreelancerServiceDetail.vue'
+
+const freelancerServiceDetailRouter = {
+  path: '/service/:id',
+  name: 'freelancer-service-detail',
+  component: FreelancerServiceDetail,
+}
+
+export default freelancerServiceDetailRouter
